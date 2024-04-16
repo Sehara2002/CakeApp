772106f49d770103ac0f2b41d40515d8ce790b9c
Imasha Nilupul <imashanilupul@gmail.com>
2024-04-16 15:50:31 +0530

    customer collection api

--- Backend/main.py
+++ Backend/main.py
@@ -1,19 +1,43 @@
-from dotenv import load_dotenv, find_dotenv
-import os
-from pymongo.mongo_client import MongoClient
-from pymongo.server_api import ServerApi
-load_dotenv(find_dotenv())
+from fastapi import FastAPI,HTTPException,status
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+from database import client
 
-password = os.environ.get("MONGODB_PWD")
 
-uri = f"mongodb+srv://cakeodyssey:{password}@cakeapp.jqdxbzo.mongodb.net/?retryWrites=true&w=majority"
+app = FastAPI()
 
-# Create a new client and connect to the server
-client = MongoClient(uri, server_api=ServerApi('1'))
 
-# Send a ping to confirm a successful connection
-try:
-    client.admin.command('ping')
-    print("Pinged your deployment. You successfully connected to MongoDB!")
-except Exception as e:
-    print(e)
+database = client["CakeApp"]
+
+class Customer_data(BaseModel):
+    name:str
+    age:int
+    gender:bool
+    address:str
+    contact_num:str
+    email:str
+    username:str
+    password:str
+
+
+
+@app.post("/signup")
+def customer(user:Customer_data):
+    collection = database["Customer"]
+    user_data = {
+        "name":user.name,
+        "age":user.age,
+        "gender":user.gender,
+        "address":user.address,
+        "contact_num":user.contact_num,
+        "email":user.email,
+        "username":user.username,
+        "password":user.password
+    }
+    inserted_id = collection.insert_one(user_data).inserted_id
+    return {"success":f"{inserted_id}"}
+
+
+    
+
+
