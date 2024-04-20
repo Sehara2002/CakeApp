76b19401bb922bb536cf43cc23689706232ca7f4
Imasha Nilupul <imashanilupul@gmail.com>
2024-04-20 12:55:06 +0530

    signup page

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,11 +1,19 @@
 import FooterComponent from "./FooterComponent"
 import Navbar from "./Navbar"
+import img6 from "./Assets/Images/backgroundcake.png"
 
 const Contact = () => {
   return (
     <div>
         <Navbar activePage = "contact"/>
-        <h1>This is Contact us Page</h1>
+        <div className="divider">
+          <div className="left">
+            <img src={img6} alt="seharacake"/>
+          </div>
+          <div className="right">
+            <h1>Butter cake</h1>
+          </div>
+        </div>
         <FooterComponent activePage = "contact"/>
     </div>
   )
