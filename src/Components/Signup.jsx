2c1364f9f3489afa1657cd1fee55ce3e6e849264
Imasha Nilupul <imashanilupul@gmail.com>
2024-04-20 10:56:08 +0530

    sign up page bug fixed

--- src/Components/Signup.jsx
+++ src/Components/Signup.jsx
@@ -12,29 +12,29 @@ function Signup() {
               <div className='column'>
                 <h1 className='title'>SignUp</h1>
                 <hr/>
-              <form>
+              <form className='form'>
                 
-                    <input type='text' id='name' name='name' placeholder='Name'/>
+                    <input type='text' id='name' name='name' placeholder='Name' className='txt'/>
                 
-                    <input type='text' id='age' name='age' value='Age' placeholder='Age'/>
+                    <input type='text' id='age' name='age'  placeholder='Age' className='txt'/>
                 
-                    <select placeholder='Gender'>
+                    <select placeholder='Gender' className='txt'>
                         <option value={true}>Male</option>
                         <option value={false}>Female</option>
                     </select>
                 
-                    <textarea placeholder='Address'></textarea>
+                    <textarea placeholder='Address' className='txt'></textarea>
                 
-                    <input type='text' id='contact' name='contact' placeholder='Contact'/>
+                    <input type='text' id='contact' name='contact' placeholder='Contact' className='txt'/>
                 
-                    <input type='email' id='email' name='email' placeholder='Email'/>
+                    <input type='email' id='email' name='email' placeholder='Email' className='txt'/>
                     <hr/>
                 
-                    <input type='text' id='usrname' name='usrnme' placeholder='Username'/>
+                    <input type='text' id='usrname' name='usrnme' placeholder='Username' className='txt'/>
                 
-                    <input type='password' id='passwd' name='passwd' placeholder='Password'/>
+                    <input type='password' id='passwd' name='passwd' placeholder='Password' className='txt'/>
                 
-                    <input type='password' id='passwwd' name='passwd' placeholder='Confirm Password'/>
+                    <input type='password' id='passwwd' name='passwd' placeholder='Confirm Password'className='txt'/>
                 <div className='subset'>
                   <div className='left1'>
                     <button>Submit</button>
