7e1f98c763345d08da17138033008d126439d3c0
Judith Fernando <judithfdo2002@gmail.com>
2024-05-01 13:17:10 +0530

    Updated Contact us Page

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -11,7 +11,6 @@ import img6 from "./Assets/Images/backgroundcake.png"
 
 const Contact = () => {
   return (
-    <div>
       <div className="contactContainer">
       <Navbar activePage = "contact"/>
       <div className="con-outer-row">
@@ -44,21 +43,11 @@ const Contact = () => {
       </div>
 
 
-
+     
       <FooterComponent activePage = "contact"/>
       </div>
-        <Navbar activePage = "contact"/>
-        <div className="divider">
-          <div className="left">
-            <img src={img6} alt="seharacake"/>
-          </div>
-          <div className="right">
-            <h1>Butter cake</h1>
-          </div>
-        </div>
-        <FooterComponent activePage = "contact"/>
-    </div>
+        
   )
 }
 
-export default Contact
+export default Contact;
