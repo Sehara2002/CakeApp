53c668bdc1cfb2a76e2faa4a1b1ea311013dfc41
Tenura Pinsara <pinsaratenura2001@gmail.com>
2024-05-03 12:07:27 +0530

    cake part 01

--- src/Components/Cakes.jsx
+++ src/Components/Cakes.jsx
@@ -1,11 +1,137 @@
 import FooterComponent from "./FooterComponent"
 import Navbar from "./Navbar"
+import "./CSS/Cake.css"
 
-const Cakes = () => {
+import wedImg from "./Assets/Images/wedding-cake.png"
+import wedImg02 from "./Assets/Images/wedding-cake02.png"
+import wedImg03 from "./Assets/Images/wedding-cake03.png"
+
+const Cakes = (props) => {
   return (
     <div>
         <Navbar activePage = "cakes"/>
-        <h1>Cake Page</h1>
+    
+        <div id="menu">
+            <h1 id="section">Menu</h1>
+            <div id="menu_row">
+                <div id="menu_col">
+                    <h2>Wedding Cake</h2>
+                    <div class="box">
+                        <div id="image">
+                         <img src={wedImg} alt="Cake Image"/>
+                        </div>
+                        <div>
+                            <h3>Tasty Dish 01</h3>
+                            <h4>10$</h4>
+                        </div>
+
+                    </div>
+
+                    <div class="box">
+                        <div id="image">
+                         <img src={wedImg02} alt="Cake Image"/>
+                        </div>
+                        <div>
+                            <h3>Tasty Dish 01</h3>
+                            <h4>10$</h4>
+                        </div>
+
+                    </div>
+
+                    <div class="box">
+                        <div id="image">
+                         <img src={wedImg03} alt="Cake Image"/>
+                        </div>
+                        <div>
+                            <h3>Tasty Dish 01</h3>
+                            <h4>10$</h4>
+                        </div>
+
+                    </div>
+
+                </div>
+
+                <div id="menu_col">
+                    <h2>Birthday Cake</h2>
+                    <div class="box">
+                        <div id="image">
+                         <img src={wedImg} alt="Cake Image"/>
+                        </div>
+                        <div>
+                            <h3>Tasty Dish 01</h3>
+                            <h4>10$</h4>
+                        </div>
+
+                    </div>
+
+                    <div class="box">
+                        <div id="image">
+                         <img src={wedImg02} alt="Cake Image"/>
+                        </div>
+                        <div>
+                            <h3>Tasty Dish 01</h3>
+                            <h4>10$</h4>
+                        </div>
+
+                    </div>
+
+                    <div class="box">
+                        <div id="image">
+                         <img src={wedImg03} alt="Cake Image"/>
+                        </div>
+                        <div>
+                            <h3>Tasty Dish 01</h3>
+                            <h4>10$</h4>
+                        </div>
+
+                    </div>
+
+                </div>
+
+                <div id="menu_col">
+                    <h2>Other</h2>
+                    <div class="box">
+                        <div id="image">
+                         <img src={wedImg} alt="Cake Image"/>
+                        </div>
+                        <div>
+                            <h3>Tasty Dish 01</h3>
+                            <h4>10$</h4>
+                        </div>
+
+                    </div>
+
+                    <div class="box">
+                        <div id="image">
+                         <img src={wedImg02} alt="Cake Image"/>
+                        </div>
+                        <div>
+                            <h3>Tasty Dish 01</h3>
+                            <h4>10$</h4>
+                        </div>
+
+                    </div>
+
+                    <div class="box">
+                        <div id="image">
+                         <img src={wedImg03} alt="Cake Image"/>
+                        </div>
+                        <div>
+                            <h3>Tasty Dish 01</h3>
+                            <h4>10$</h4>
+                        </div>
+
+                    </div>
+
+                </div>
+            </div>
+
+        </div>
+       
+        
+
+
+
         <FooterComponent activePage = "cakes"/>
     </div>
   )
