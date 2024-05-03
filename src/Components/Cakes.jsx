4766426c65710fbf4b4ae47323b58920beb3bd1b
Tenura Pinsara <pinsaratenura2001@gmail.com>
2024-05-03 13:00:12 +0530

    cake page change 02

--- src/Components/Cakes.jsx
+++ src/Components/Cakes.jsx
@@ -5,6 +5,13 @@ import "./CSS/Cake.css"
 import wedImg from "./Assets/Images/wedding-cake.png"
 import wedImg02 from "./Assets/Images/wedding-cake02.png"
 import wedImg03 from "./Assets/Images/wedding-cake03.png"
+import BriImg01 from "./Assets/Images/BirthdayCake-01.png"
+import BriImg02 from "./Assets/Images/BirthdayCake-02.jpg"
+import OtherImg01 from "./Assets/Images/Cupcake.png"
+import OtherImg02 from "./Assets/Images/donut.png"
+import OtherImg03 from "./Assets/Images/Butter-Cake-1.jpg"
+
+
 
 const Cakes = (props) => {
   return (
@@ -12,7 +19,7 @@ const Cakes = (props) => {
         <Navbar activePage = "cakes"/>
     
         <div id="menu">
-            <h1 id="section">Menu</h1>
+        <h1 id="section"></h1>
             <div id="menu_row">
                 <div id="menu_col">
                     <h2>Wedding Cake</h2>
@@ -20,8 +27,8 @@ const Cakes = (props) => {
                         <div id="image">
                          <img src={wedImg} alt="Cake Image"/>
                         </div>
-                        <div>
-                            <h3>Tasty Dish 01</h3>
+                        <div class="pri">
+                            <h3>Design 01</h3>
                             <h4>10$</h4>
                         </div>
 
@@ -31,8 +38,8 @@ const Cakes = (props) => {
                         <div id="image">
                          <img src={wedImg02} alt="Cake Image"/>
                         </div>
-                        <div>
-                            <h3>Tasty Dish 01</h3>
+                        <div class="pri">
+                            <h3>Design 02</h3>
                             <h4>10$</h4>
                         </div>
 
@@ -42,8 +49,8 @@ const Cakes = (props) => {
                         <div id="image">
                          <img src={wedImg03} alt="Cake Image"/>
                         </div>
-                        <div>
-                            <h3>Tasty Dish 01</h3>
+                        <div class="pri">
+                            <h3>Design 03</h3>
                             <h4>10$</h4>
                         </div>
 
@@ -55,10 +62,10 @@ const Cakes = (props) => {
                     <h2>Birthday Cake</h2>
                     <div class="box">
                         <div id="image">
-                         <img src={wedImg} alt="Cake Image"/>
+                         <img src={BriImg01} alt="Cake Image"/>
                         </div>
-                        <div>
-                            <h3>Tasty Dish 01</h3>
+                        <div class="pri">
+                            <h3>Design 01</h3>
                             <h4>10$</h4>
                         </div>
 
@@ -66,10 +73,10 @@ const Cakes = (props) => {
 
                     <div class="box">
                         <div id="image">
-                         <img src={wedImg02} alt="Cake Image"/>
+                         <img src={BriImg02} alt="Cake Image"/>
                         </div>
-                        <div>
-                            <h3>Tasty Dish 01</h3>
+                        <div class="pri">
+                            <h3>Design 02</h3>
                             <h4>10$</h4>
                         </div>
 
@@ -77,10 +84,10 @@ const Cakes = (props) => {
 
                     <div class="box">
                         <div id="image">
-                         <img src={wedImg03} alt="Cake Image"/>
+                         <img src={BriImg01} alt="Cake Image"/>
                         </div>
-                        <div>
-                            <h3>Tasty Dish 01</h3>
+                        <div class="pri">
+                            <h3>Design 03</h3>
                             <h4>10$</h4>
                         </div>
 
@@ -92,10 +99,10 @@ const Cakes = (props) => {
                     <h2>Other</h2>
                     <div class="box">
                         <div id="image">
-                         <img src={wedImg} alt="Cake Image"/>
+                         <img src={OtherImg01} alt="Cake Image"/>
                         </div>
-                        <div>
-                            <h3>Tasty Dish 01</h3>
+                        <div class="pri">
+                            <h3>Design 01</h3>
                             <h4>10$</h4>
                         </div>
 
@@ -103,10 +110,10 @@ const Cakes = (props) => {
 
                     <div class="box">
                         <div id="image">
-                         <img src={wedImg02} alt="Cake Image"/>
+                         <img src={OtherImg02} alt="Cake Image"/>
                         </div>
-                        <div>
-                            <h3>Tasty Dish 01</h3>
+                        <div class="pri">
+                            <h3>Design 02</h3>
                             <h4>10$</h4>
                         </div>
 
@@ -114,10 +121,10 @@ const Cakes = (props) => {
 
                     <div class="box">
                         <div id="image">
-                         <img src={wedImg03} alt="Cake Image"/>
+                         <img src={OtherImg03} alt="Cake Image"/>
                         </div>
-                        <div>
-                            <h3>Tasty Dish 01</h3>
+                        <div class="pri">
+                            <h3>Design 03</h3>
                             <h4>10$</h4>
                         </div>
 
