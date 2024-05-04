8cdbaef5a1e34634e5d90b7f39d12a9fd76b0e59
Imasha Nilupul <imashanilupul@gmail.com>
2024-05-04 16:06:55 +0530

    something

--- src/Components/Signup.jsx
+++ src/Components/Signup.jsx
@@ -1,52 +1,39 @@
 import React, {useState, useEffect} from 'react'
 import api from "../../api/api"
 import "./CSS/Signup.css"
+import { useNavigate } from 'react-router-dom'
 import img5 from "./Assets/Images/cupcake.jpg"
+import axios from 'axios'
+
+function SignUp(){
+  const [name, setName] = useState('');
+  const [age, setAge] = useState(0);
+  const [gender, setGender] = useState('Male');
+  const [address, setAddress] = useState('');
+  const [contact, setContact] = useState('');
+  const [email, setEmail] = useState('');
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  //message
+  const [message, setMessage] = useState('');
+  const navigate = useNavigate();
+
+  const handleSignup = () =>{
+    axios.post('http://localhost:8000/signup', {name,age,gender,address,contact,email,username,password})
+    .then(response => {
+      setMessage(response.data.message);
+      const {name, age, gender, address, contact, email, username, password,token} = response.data;
+    })
+    .catch(error =>{
+      console.error(error);
+      setMessage("Error try again later");
+    });
+  }
+};
+
+
 
 function Signup() {
-  const [user, setUser] = useState([]);
-  const [formData, setFormData] = useState({
-    name:'',
-    age:'',
-    gender:'',
-    address:'',
-    contact:'',
-    email:'',
-    username:'',
-    password:''
-  });
-
-  const fetchUser = async()=>{
-    const response = await api.get('/signup');
-    setUser(response.data);
-  }
-
-  useEffect(()=>{
-    fetchUser();
-  },[]);
-
-  const handleInputChange = (event) =>{
-    setFormData({
-      ...formData,
-      [event.target.name]:value,
-    });
-  };
-
-  const handleFormSubmit = async(event) => {
-    event.preventDefault();
-    await api.post('/signup', formData);
-    fetchUser();
-    setFormData({
-    name:'',
-    age:'',
-    gender:'',
-    address:'',
-    contact:'',
-    email:'',
-    username:'',
-    password:''
-    });
-  };
   return (
     <><div className='container'>
           <div className='row'>
@@ -56,43 +43,43 @@ function Signup() {
               <div className='column'>
                 <h1 className='title'>SignUp</h1>
                 <hr color='darkgoldenrod'/>
-              <form className='form'>
+              <form className='form' onSubmit={handleSignup}>
                 <div className='borderupper'>
                   <div className='mar'>
                 
-                    <input type='text' id='name' name='name' placeholder='Name' className='txt1' onChange={handleInputChange} />
+                    <input type='text' id='name' name='name' placeholder='Name' className='txt1' value={name} onChange={e=>setName(e.target.value)}/>
                 
-                    <input type='text' id='age' name='age'  placeholder='Age' className='txt1' onChange={handleInputChange} />
+                    <input type='text' id='age' name='age'  placeholder='Age' className='txt1'value={age} onChange={e=>setAge(e.target.value)}/>
                 
-                    <select placeholder='Gender' className='txt1' onChange={handleInputChange} >
+                    <select placeholder='Gender' className='txt1' value={gender} onChange={e=>setGender(e.target.value)}>
                         <option value="Male">Male</option>
                         <option value="Female">Female</option>
                     </select>
                 
-                    <textarea placeholder='Address' className='txt1' rows='6' onChange={handleInputChange} ></textarea>
+                    <textarea placeholder='Address' className='txt1' rows='6' value={address} onChange={e=>setAddress(e.target.value)}></textarea>
                 
-                    <input type='text' id='contact' name='contact' placeholder='Contact' className='txt1'onChange={handleInputChange} />
+                    <input type='text' id='contact' name='contact' placeholder='Contact' className='txt1' value={contact} onChange={e=>setContact(e.target.value)}/>
                 
-                    <input type='email' id='email' name='email' placeholder='Email' className='txt1' onChange={handleInputChange} />
+                    <input type='email' id='email' name='email' placeholder='Email' className='txt1' value={email} onChange={e=>setEmail(e.target.value)}/>
                     </div>
                     </div>
                     <hr color='darkgoldenrod'/>
                     <div className='borderupper'>
                     <div className='mar'>
                 
-                    <input type='text' id='usrname' name='usrnme' placeholder='Username' className='txt1' onChange={handleInputChange} />
+                    <input type='text' id='usrname' name='usrnme' placeholder='Username' className='txt1' value={username} onChange={e=>setUsername(e.target.value)}/>
                 
-                    <input type='password' id='passwd' name='passwd' placeholder='Password' className='txt1' onChange={handleInputChange} />
+                    <input type='password' id='passwd' name='passwd' placeholder='Password' className='txt1' value={password} onChange={e=>setPassword(e.target.value)}/>
                 
                     <input type='password' id='passwwd' name='passwd' placeholder='Confirm Password'className='txt1'/>
                     </div>
                     </div>
                 <div className='subset'>
                   <div className='left1'>
-                    <button onSubmit={handleFormSubmit}>Submit</button>
+                    <input type='submit'/>
                   </div>
                   <div className='right2'>
-                    <button>Reset</button>
+                    <input type='reset'/>
                   </div>
                   
                 </div>
